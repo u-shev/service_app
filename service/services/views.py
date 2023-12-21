f1121113aeba476bb4dc654fa0bc46d6f1599199
u-shev <sheveleva.jul@gmail.com>
2023-12-21 15:45:34 +0300

    added total amount counting

--- service/services/views.py
+++ service/services/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from clients.models import Client
 
 
@@ -10,5 +10,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
                                                               'user__email'))
-    )
+    ).annotate(price=F('service__full_price') -
+                     F('service__full_price') * F('plan__discount_percent') / 100.00
+               )
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+        return response
